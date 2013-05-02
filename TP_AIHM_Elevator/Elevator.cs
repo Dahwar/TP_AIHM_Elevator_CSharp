3532636625cb9cc7186ace21b18ce1281b6342a1
Dahwar <dahwar2713@gmail.com>
2013-05-02 15:42:23 +0200

    Animation Elevator
    Animation Elevator and change method to add Elevator to the Window

--- TP_AIHM_Elevator/Elevator.cs
+++ TP_AIHM_Elevator/Elevator.cs
@@ -11,24 +11,44 @@ namespace TP_AIHM_Elevator
     {
         private LinkedList<int> listFloor = new LinkedList<int>();
 
+        private Timer timerElevator;
+        private Timer timerPause;
+
+        private enum CabinMoves { UP, DOWN, STAY };
+        private CabinMoves currentMove = CabinMoves.STAY;
+        private int currentPosition = 0;
+
+        private int coef = 1000;
+
         Pen grayPen = new Pen(Color.Gray, 2);
         Pen blackPen = new Pen(Color.Black, 1);
         SolidBrush gray = new SolidBrush(Color.Gray);
 
+    
+
         public Elevator()
         {
+            this.Size = new Size(160, 540);
+            this.Paint += new PaintEventHandler(AddDraw);
+            this.DoubleBuffered = true;
 
+            this.timerElevator = new Timer();
+            this.timerElevator.Interval = 10;
+            this.timerElevator.Tick += new EventHandler(this.TimerEvent);
+            this.timerElevator.Start();
         }
 
-        public void AddDraw(Graphics g)
+        private void AddDraw(object sender, PaintEventArgs e)
         {
-            g.DrawLine(this.blackPen, 50, 80, 50, 530);
-            g.DrawLine(this.blackPen, 150, 80, 150, 530);
+            Graphics g = e.Graphics;
 
-            g.FillRectangle(this.gray, 52, 80, 48, 150);
-            g.FillRectangle(this.gray, 101, 80, 48, 150);
+            double positionY = 0.3 * coef;
 
-            g.Dispose();
+            g.DrawLine(this.blackPen, 50, 80, 50, 529);
+            g.DrawLine(this.blackPen, 150, 80, 150, 529);
+
+            g.FillRectangle(this.gray, 52, (int)positionY+80, 48, 150);
+            g.FillRectangle(this.gray, 101, (int)positionY+80, 48, 150);
         }
 
         public void AddFloorToList(int floor)
@@ -41,6 +61,79 @@ namespace TP_AIHM_Elevator
                     //HMButtons.get(floor).setSelected(true);
                 }
             }
+        }
+
+        private void TimerEvent(Object sender, EventArgs e)
+        {
+            switch(this.currentMove){
+				case CabinMoves.STAY:
+					if(this.listFloor.Count!=0){
+						if(this.listFloor.First.Value != this.currentPosition){
+                            if (this.listFloor.First.Value > currentPosition)
+                                this.currentMove = CabinMoves.UP;
+                            else if (this.listFloor.First.Value < currentPosition)
+                                this.currentMove = CabinMoves.DOWN;
+                            else
+                                this.listFloor.RemoveFirst();
+						}
+						else
+                            this.listFloor.RemoveFirst();
+							//HMButtons.get(currentPosition).setSelected(false);
+					}
+					break;
+						
+				case CabinMoves.UP:
+					coef-=5;
+					if(coef==0){
+						currentPosition=2;
+						//myWindow.setCabinLevel(currentPosition);
+					}
+					else if(coef==500){
+						currentPosition=1;
+						//myWindow.setCabinLevel(currentPosition);
+					}
+					else if(coef==1000){
+						currentPosition=0;
+						//myWindow.setCabinLevel(currentPosition);
+					}
+					else;
+						
+					if(this.listFloor.First.Value==currentPosition){
+						currentMove = CabinMoves.STAY;
+						//HMButtons.get(currentPosition).setSelected(false);
+						//this.timerElevator.Stop();
+						//this.timerPause.Start();
+                        this.listFloor.RemoveFirst();
+					}
+					break;
+						
+				case CabinMoves.DOWN:
+					coef+=5;
+					if(coef==0){
+						currentPosition=2;
+						//myWindow.setCabinLevel(currentPosition);
+					}
+					else if(coef==500){
+						currentPosition=1;
+						//myWindow.setCabinLevel(currentPosition);
+					}
+					else if(coef==1000){
+						currentPosition=0;
+						//myWindow.setCabinLevel(currentPosition);
+					}
+					else;
+						
+					if(this.listFloor.First.Value==currentPosition){
+						currentMove = CabinMoves.STAY;
+						//HMButtons.get(currentPosition).setSelected(false);
+						//timer.stop();
+						//timerPause.start();
+                        this.listFloor.RemoveFirst();
+					}
+					break;
+			}
+            //Console.Write(coef);
+            this.Invalidate();
         }
     }
 }
--- TP_AIHM_Elevator/Form1.cs
+++ TP_AIHM_Elevator/Form1.cs
@@ -11,25 +11,18 @@ namespace TP_AIHM_Elevator
 {
     public partial class Form1 : Form
     {
-        Elevator elevator;
-        Timer timer = new Timer();
+        private Elevator elevator;
 
         public Form1()
         {
             InitializeComponent();
             this.elevator = new Elevator();
-            this.Paint += new PaintEventHandler(AddDraw);
-
+            this.Controls.Add(this.elevator);
         }
 
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
-        }
-
-        private void AddDraw(object sender, PaintEventArgs e)
-        {
-            elevator.AddDraw(e.Graphics);
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
