6d129729948d5b30942b96241f5f7613870a46a2
Dahwar <dahwar2713@gmail.com>
2013-05-05 19:08:18 +0200

    Animate Door
    The door of the elevator open and close, with the help of a state
    machine

--- TP_AIHM_Elevator/Elevator.cs
+++ TP_AIHM_Elevator/Elevator.cs
@@ -14,6 +14,7 @@ namespace TP_AIHM_Elevator
         CheckBox recupButton;
 
         private Timer timerElevator;
+        private Timer timerDoor;
         private Timer timerPause;
 
         private enum CabinMoves { UP, DOWN, STAY };
@@ -21,7 +22,11 @@ namespace TP_AIHM_Elevator
         private int currentPosition = 0;
 
         private int coef = 1000;
+
+        private enum CabinDoorState { OPENING, CLOSING, OPEN, CLOSE };
+        private CabinDoorState currentDoorState = CabinDoorState.CLOSE;
         private int doorSizeX = 48;
+        private int positionXRightDoor = 101;
 
         Pen grayPen = new Pen(Color.Gray, 2);
         Pen blackPen = new Pen(Color.Black, 1);
@@ -42,6 +47,10 @@ namespace TP_AIHM_Elevator
             this.timerPause = new Timer();
             this.timerPause.Interval = 500;
             this.timerPause.Tick += new EventHandler(this.TimerEventPause);
+
+            this.timerDoor = new Timer();
+            this.timerDoor.Interval = 20;
+            this.timerDoor.Tick += new EventHandler(this.TimerEventDoor);
         }
 
         private void AddDraw(object sender, PaintEventArgs e)
@@ -54,7 +63,7 @@ namespace TP_AIHM_Elevator
             g.DrawLine(this.blackPen, 150, 80, 150, 529);
 
             g.FillRectangle(this.gray, 52, (int)positionY+80, this.doorSizeX, 150);
-            g.FillRectangle(this.gray, 101, (int)positionY + 80, this.doorSizeX, 150);
+            g.FillRectangle(this.gray, this.positionXRightDoor, (int)positionY + 80, this.doorSizeX, 150);
         }
 
         public void AddFloorToList(int floor)
@@ -73,83 +82,133 @@ namespace TP_AIHM_Elevator
 
         private void TimerEventElevator(Object sender, EventArgs e)
         {
-            switch(this.currentMove){
-				case CabinMoves.STAY:
-					if(this.listFloor.Count!=0){
-						if(this.listFloor.First.Value != this.currentPosition){
-                            if (this.listFloor.First.Value > currentPosition)
-                                this.currentMove = CabinMoves.UP;
-                            else if (this.listFloor.First.Value < currentPosition)
-                                this.currentMove = CabinMoves.DOWN;
+            if (this.currentDoorState == CabinDoorState.CLOSE)
+            {
+                switch (this.currentMove)
+                {
+                    case CabinMoves.STAY:
+                        if (this.listFloor.Count != 0)
+                        {
+                            if (this.listFloor.First.Value != this.currentPosition)
+                            {
+                                if (this.listFloor.First.Value > currentPosition)
+                                    this.currentMove = CabinMoves.UP;
+                                else if (this.listFloor.First.Value < currentPosition)
+                                    this.currentMove = CabinMoves.DOWN;
+                                else
+                                    this.listFloor.RemoveFirst();
+                            }
                             else
                                 this.listFloor.RemoveFirst();
-						}
-						else
-                            this.listFloor.RemoveFirst();
                             this.listButton.TryGetValue(this.currentPosition, out this.recupButton);
                             this.recupButton.Checked = false;
                             this.recupButton = null;
-					}
-					break;
-						
-				case CabinMoves.UP:
-					coef-=5;
-					if(coef==0){
-						currentPosition=2;
-					}
-					else if(coef==500){
-						currentPosition=1;
-					}
-					else if(coef==1000){
-						currentPosition=0;
-					}
-					else;
-						
-					if(this.listFloor.First.Value==currentPosition){
-						currentMove = CabinMoves.STAY;
-                        this.listButton.TryGetValue(this.currentPosition, out this.recupButton);
-                        this.recupButton.Checked = false;
-                        this.recupButton = null;
-						this.timerElevator.Stop();
-						this.timerPause.Start();
-                        this.listFloor.RemoveFirst();
-					}
-					break;
-						
-				case CabinMoves.DOWN:
-					coef+=5;
-                    if (coef == 0)
+                        }
+                        break;
+
+                    case CabinMoves.UP:
+                        coef -= 5;
+                        if (coef == 0)
+                        {
+                            currentPosition = 2;
+                        }
+                        else if (coef == 500)
+                        {
+                            currentPosition = 1;
+                        }
+                        else if (coef == 1000)
+                        {
+                            currentPosition = 0;
+                        }
+                        else ;
+
+                        if (this.listFloor.First.Value == currentPosition)
+                        {
+                            currentMove = CabinMoves.STAY;
+                            this.listButton.TryGetValue(this.currentPosition, out this.recupButton);
+                            this.recupButton.Checked = false;
+                            this.recupButton = null;
+                            this.timerElevator.Stop();
+                            this.currentDoorState = CabinDoorState.OPENING;
+                            this.timerPause.Start();
+                            this.listFloor.RemoveFirst();
+                        }
+                        break;
+
+                    case CabinMoves.DOWN:
+                        coef += 5;
+                        if (coef == 0)
+                        {
+                            currentPosition = 2;
+                        }
+                        else if (coef == 500)
+                        {
+                            currentPosition = 1;
+                        }
+                        else if (coef == 1000)
+                        {
+                            currentPosition = 0;
+                        }
+                        else ;
+
+                        if (this.listFloor.First.Value == currentPosition)
+                        {
+                            currentMove = CabinMoves.STAY;
+                            this.listButton.TryGetValue(this.currentPosition, out this.recupButton);
+                            this.recupButton.Checked = false;
+                            this.recupButton = null;
+                            this.timerElevator.Stop();
+                            this.currentDoorState = CabinDoorState.OPENING;
+                            this.timerPause.Start();
+                            this.listFloor.RemoveFirst();
+                        }
+                        break;
+                }
+                this.Invalidate();
+            }
+        }
+
+        private void TimerEventDoor(Object sender, EventArgs e)
+        {
+            switch (this.currentDoorState)
+            {
+                case CabinDoorState.CLOSE:
+                    this.timerDoor.Stop();
+                    this.timerElevator.Start();
+                    break;
+                case CabinDoorState.CLOSING:
+                    this.doorSizeX++;
+                    this.positionXRightDoor--;
+                    if (this.doorSizeX >= 48)
                     {
-                        currentPosition = 2;
-                    }
-                    else if (coef == 500)
-                    {
-                        currentPosition = 1;
-                    }
-                    else if (coef == 1000)
-                    {
-                        currentPosition = 0;
-                    }
-                    else;
-						
-					if(this.listFloor.First.Value==currentPosition){
-						currentMove = CabinMoves.STAY;
-                        this.listButton.TryGetValue(this.currentPosition, out this.recupButton);
-                        this.recupButton.Checked = false;
-                        this.recupButton = null;
-                        this.timerElevator.Stop();
+                        this.currentDoorState = CabinDoorState.CLOSE;
+                        this.timerDoor.Stop();
                         this.timerPause.Start();
-                        this.listFloor.RemoveFirst();
-					}
-					break;
-			}
+                    }
+                    break;
+                case CabinDoorState.OPEN:
+                    this.currentDoorState = CabinDoorState.CLOSING;
+                    this.timerDoor.Stop();
+                    this.timerPause.Start();
+                    break;
+                case CabinDoorState.OPENING:
+                    this.doorSizeX--;
+                    this.positionXRightDoor++;
+                    if (this.doorSizeX <= 0)
+                    {
+                        this.currentDoorState = CabinDoorState.OPEN;
+                        this.timerDoor.Stop();
+                        this.timerPause.Start();
+                    }
+                    break;
+            }
             this.Invalidate();
         }
 
         private void TimerEventPause(Object sender, EventArgs e)
         {
             this.timerPause.Stop();
-            this.timerElevator.Start();
+            this.timerDoor.Start();
         }
 
         public int getCurrentFloor()
